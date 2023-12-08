25aa132d0e4ceac779e460e0b87c589aa63eff42
Tikhon26 <150147662+Tikhon26@users.noreply.github.com>
2023-12-08 15:51:50 +0200

    adaptive mobile version nedodelanaya

--- main.js
+++ main.js
@@ -11,4 +11,25 @@ activeBtn.classList.remove('active');
 let classLink='.main-link'
 window.onscroll = function() {
     let h = document.documentElement.clientHeight;
+
+if (window.scrollY >= h*4 ) {
+     classLink = '.comments-link';
+}
+else if (window.scrollY = h*3) {
+     classLink = '.works-link';
+}
+else if (window.scrollY >h*2 ){
+     classLink = '.skills-link';
+}
+else if (window.scrollY = h) {
+     classLink = '.about-link';
+}
+else{
+     classLink = '.main-link';
+}
+let activeBtn = document.querySelector('.active');
+let newActiveBtn = document.querySelector (classLink);
+if (!newActiveBtn.classList.contains('active')) {
+     newActiveBtn.classList.add('active'); activeBtn.classList.remove('active')
+};
 }
