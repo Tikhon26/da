141f599d52ffcc985fe2b424502363307052b001
Tikhon26 <150147662+Tikhon26@users.noreply.github.com>
2023-12-26 15:31:58 +0200

    cones

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let mobilebutton = document.querySelector ('.mobile-button')
+mobilebutton.addEventListener('click',
+function (event) {
+     document.querySelector(".mobile-menu").classList.toggle("hide");
+});
 let menu = document.querySelector(".fixed-menu");
 
 menu.addEventListener('click', function (event) {
@@ -7,7 +12,20 @@ let activeBtn = document.querySelector('.active');
 if (clickTarget.classList.contains('nav-link')){
 clickTarget.classList.add('active');
 activeBtn.classList.remove('active');
-} });
+let mobile = document.querySelector('.mobile-menu');
+if(!mobile.classList.contains('hide')){
+     mobile.classList.add('hide');
+}}
+let body = document.querySelector("body");
+let mobileMenu = document.querySelector(".mobile-menu"); 
+if (!body.classList.contains("off-scroll") &&
+!mobileMenu.classList.contains('hide')) {
+      body.classList.add("off-scroll");
+}
+else{
+body.classList.remove("off-scroll");
+}
+} );
 let classLink='.main-link'
 window.onscroll = function() {
     let h = document.documentElement.clientHeight;
@@ -30,6 +48,8 @@ else{
 let activeBtn = document.querySelector('.active');
 let newActiveBtn = document.querySelector (classLink);
 if (!newActiveBtn.classList.contains('active')) {
-     newActiveBtn.classList.add('active'); activeBtn.classList.remove('active')
-};
+     newActiveBtn.classList.add('active'); 
+     activeBtn.classList.remove('active')
 }
+};
+
